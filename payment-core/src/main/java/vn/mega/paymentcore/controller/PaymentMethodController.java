082ebcb58d277488c0c04e819ff1c31039b23c5c
Thuong Vo Hoai <hoaithuong.data@gmail.com>
2024-01-10 20:09:47 +0700

    ++ update web + add swagger

--- payment-core/src/main/java/vn/mega/paymentcore/controller/PaymentMethodController.java
+++ payment-core/src/main/java/vn/mega/paymentcore/controller/PaymentMethodController.java
@@ -1,8 +1,43 @@
 package vn.mega.paymentcore.controller;
 
 
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vn.mega.coreconfig.utils.Const;
+import vn.mega.paymentcore.entities.Payment;
+import vn.mega.paymentcore.entities.PaymentMethod;
+import vn.mega.paymentcore.service.PaymentMethodService;
 
 @RestController
+@RequestMapping(Const.API_VERSION + "/payment-methods")
 public class PaymentMethodController {
+
+    @Autowired
+    private PaymentMethodService paymentMethodService;
+
+    @GetMapping("/{tenantId}")
+    public ResponseEntity<?> getPayments(@PathVariable @NotEmpty String tenantId, Pageable pageable) {
+        // Set default pagination values if not provided
+        if (pageable == null) {
+            pageable = PageRequest.of(0, 10); // Page 1 with 10 records by default
+        }
+
+        Page<PaymentMethod> paymentMethods = paymentMethodService.getPaymentMethods(tenantId, pageable);
+
+        // Check if payments exist for the given tenantId
+        if (paymentMethods.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(paymentMethods);
+    }
+
 }
--- payment-web/src/main/java/vn/mega/paymentweb/controller/PaymentBean.java
+++ payment-web/src/main/java/vn/mega/paymentweb/controller/PaymentBean.java
@@ -1,61 +1,61 @@
-package vn.mega.paymentweb.controller;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import vn.mega.coreconfig.utils.UserUtils;
-import vn.mega.paymentcore.entities.Payment;
-import vn.mega.paymentcore.service.PaymentService;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-@Controller
-public class PaymentBean {
-
-
-    @Autowired
-    private PaymentService paymentService;
-
-    @RequestMapping(value = {"/payment"}, method = RequestMethod.GET)
-    public String getPayment(Model model, @RequestParam("page") Optional<Integer> page,
-                             @RequestParam("size") Optional<Integer> size) {
-        int currentPage = page.orElse(1);
-        int pageSize = size.orElse(5);
-
-        Page<Payment> payments = paymentService.getPayments(UserUtils.getSelectedTenant(), PageRequest.of(currentPage - 1, pageSize));
-
-        model.addAttribute("payments", payments);
-
-        int totalPages = payments.getTotalPages();
-        if (totalPages > 0) {
-            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
-                    .boxed()
-                    .collect(Collectors.toList());
-            model.addAttribute("pageNumbers", pageNumbers);
-        }
-
-        return "payment";
-    }
-
-
-    @GetMapping("/addPayment")
-    public String showPaymentForm(Model model) {
-        model.addAttribute("payment", new Payment());
-        return "payment-method-detail"; // this corresponds to your Thymeleaf template name
-    }
-
-
-//    @PostMapping("/savePayment")
-//    public String savePayment(@ModelAttribute Payment payment) {
-//        paymentService.createPayment(payment);
-//        return "redirect:/addPayment"; // redirect to the form page after saving
+//package vn.mega.paymentweb.controller;
+//
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//import vn.mega.coreconfig.utils.UserUtils;
+//import vn.mega.paymentcore.entities.Payment;
+//import vn.mega.paymentcore.service.PaymentService;
+//
+//import java.util.List;
+//import java.util.Optional;
+//import java.util.stream.Collectors;
+//import java.util.stream.IntStream;
+//
+//@Controller
+//public class PaymentBean {
+//
+//
+//    @Autowired
+//    private PaymentService paymentService;
+//
+//    @RequestMapping(value = {"/payment"}, method = RequestMethod.GET)
+//    public String getPayment(Model model, @RequestParam("page") Optional<Integer> page,
+//                             @RequestParam("size") Optional<Integer> size) {
+//        int currentPage = page.orElse(1);
+//        int pageSize = size.orElse(5);
+//
+//        Page<Payment> payments = paymentService.getPayments(UserUtils.getSelectedTenant(), PageRequest.of(currentPage - 1, pageSize));
+//
+//        model.addAttribute("payments", payments);
+//
+//        int totalPages = payments.getTotalPages();
+//        if (totalPages > 0) {
+//            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+//                    .boxed()
+//                    .collect(Collectors.toList());
+//            model.addAttribute("pageNumbers", pageNumbers);
+//        }
+//
+//        return "payment";
 //    }
-
-}
+//
+//
+//    @GetMapping("/addPayment")
+//    public String showPaymentForm(Model model) {
+//        model.addAttribute("payment", new Payment());
+//        return "payment-method-detail"; // this corresponds to your Thymeleaf template name
+//    }
+//
+//
+////    @PostMapping("/savePayment")
+////    public String savePayment(@ModelAttribute Payment payment) {
+////        paymentService.createPayment(payment);
+////        return "redirect:/addPayment"; // redirect to the form page after saving
+////    }
+//
+//}
